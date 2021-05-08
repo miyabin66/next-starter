9b2d371a5328dee2db6b06650b06f8fa17f46823
miyabin66 <freezemark5166@gmail.com>
2021-05-08 23:14:58 +0900

    api使わない

--- pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Request, Response } from 'express'
-
-export default (_req: Request, res: Response): void => {
-  res.status(200).json({ name: 'John Doe' })
-}
