9861395d02114aa28e4a3e15f3bd5374dd634fa6
miyabin66 <freezemark5166@gmail.com>
2021-05-09 11:32:31 +0900

    readme通りに実装

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const withPlugins = import('next-compose-plugins')
-const optimizedImages = import('next-optimized-images')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
 
 module.exports = withPlugins([
   [
