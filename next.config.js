c73514c58eb1d23e92e3e3531f714e0092005c87
miyabin66 <freezemark5166@gmail.com>
2021-05-09 11:03:05 +0900

    画像圧縮設定追加

---
+++ next.config.js
@@ -0,0 +1,34 @@
+import withPlugins from 'next-compose-plugins'
+import optimizedImages from 'next-optimized-images'
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      optimizeImages: true,
+      mozjpeg: {
+        quality: 50,
+      },
+      pngquant: {
+        quality: [0.3, 0.5],
+      },
+      gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+      },
+      svgo: {
+        options: {
+          multipass: true,
+          datauri: 'enc',
+          js2svg: {
+            indent: 2,
+            pretty: true,
+          },
+        },
+      },
+      webp: {
+        quality: 50,
+      },
+    },
+  ],
+])
