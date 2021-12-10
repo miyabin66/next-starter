dca3cc6dabe9a077d3f84772556164b4a4b20254
miyabin66 <freezemark5166@gmail.com>
2021-12-10 11:09:29 +0900

    いらないものを消す

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-const path = require('path')
 
 module.exports = withPlugins([
   {
@@ -8,33 +6,4 @@ module.exports = withPlugins([
       prependData: '@import "~/styles/commons.scss";',
     },
   },
-  [
-    optimizedImages,
-    {
-      optimizeImages: true,
-      mozjpeg: {
-        quality: 50,
-      },
-      pngquant: {
-        quality: [0.3, 0.5],
-      },
-      gifsicle: {
-        interlaced: true,
-        optimizationLevel: 3,
-      },
-      svgo: {
-        options: {
-          multipass: true,
-          datauri: 'enc',
-          js2svg: {
-            indent: 2,
-            pretty: true,
-          },
-        },
-      },
-      webp: {
-        quality: 50,
-      },
-    },
-  ],
 ])
